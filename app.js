9c4008ebdec59b5fe662b69fa08aaf4527a5ad93
Lorenzo Fontana <fontanalorenz@gmail.com>
2016-08-22 16:19:37 +0200

    Some code to retrieve the captain location

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 
 let cheerio = require('cheerio');
 let Parser = require('./parser');
+let Location = require('./location');
 let request = require('request');
 let fs = require('fs');
 
 let debug = process.argv.length > 2 && process.argv[2] == "--debug"
 
 let parser = new Parser(cheerio);
+let location = new Location(cheerio, request);
+
 // fs.readFile("./docker-captains.html", "utf8", (err, text)=>{
 request.get("https://www.docker.com/community/docker-captains", (err, res, text) => {
   let captains = parser.parse(text);
@@ -26,7 +29,12 @@ request.get("https://www.docker.com/community/docker-captains", (err, res, text)
      return 0;
   }).forEach((cap) => {
     if(cap.valid) {
-      console.log(cap.text);
+      location.retrieve(cap.text, function(location) {
+        if (!location) {
+          location = "undefined";
+        }
+        console.log(cap.text + " - " + location);
+      });
       valid++;
     }
   });
