d9cba2b2b5cc911b8e46d1eee50ee023a2549435
Alex <alexellis2@gmail.com>
2016-08-23 09:00:21 +0100

    Merge changes from fntlnz

--- app.js
+++ app.js
@@ -5,67 +5,76 @@ let Parser = require('./parser');
 let Location = require('./location');
 let request = require('request');
 let fs = require('fs');
+let optionsReader = require('./optionsReader');
 
 let debug = false;
 
-let modeCallback = (cap) => console.log(cap.text);
+let options = optionsReader(process.argv);
 
-process.argv.map(function(elem) {
-  let parts = elem.split("=");
-  if (parts[0] === "--debug") {
-    debug = true;
-  }
+if(options.help) {
+  let helpStr = `captains:
+  --debug                       Print additional information
+  --mode=list                   Set the output mode: available modes are 
+                                  <list,locations>
+  --help                        This information`
+  console.log(helpStr); 
+  return;
+}
 
-  if (parts[0] === "--mode" && parts[1] === "location") {
-    modeCallback = function(cap) {
-      location.retrieve(cap.text, function(location) {
-        if (!location) {
-          location = "undefined";
+let createList = (next, done) => {
+  let parser = new Parser(cheerio);
+
+  request.get("https://www.docker.com/community/docker-captains", (err, res, text) => {
+    let captains = parser.parse(text);
+
+    let valid = 0;
+    let sorted = captains.sort((x,y) => {
+      if(x.text > y.text) {
+        return 1;
+      }
+      else if(x.text < y.text) {
+        return -1;
+      }
+      return 0;
+    });
+    next(sorted, done);
+  });
+};
+
+let printer = (captains, done) => {
+  captains.forEach((cap)=> {
+    if(cap.valid) {
+      console.log(cap.text);
+    }
+    done();
+  });
+}
+
+let locationPrinter = (captains, done) => {
+  let location = new Location(cheerio, request);
+  let work = captains.length;
+  captains.forEach((cap)=> {
+    if(cap.valid) {
+      location.retrieve(cap.text, (details) => {
+        console.log(cap.text + ": " +  details);
+        work--;
+        if(work==0) {
+          return;
         }
-        console.log(cap.text + " - " + location);
       });
     }
-  }
-
-  if (parts[0] === "--help") {
-    let helpStr = `captains:
-    --debug                       Print additional information
-    --mode=default                Set the output mode: available modes are <default,location>
-    --help                        Guess what!`
-    console.log(helpStr);
-    process.exit()
-  }
-
-});
-
-let parser = new Parser(cheerio);
-let location = new Location(cheerio, request);
-
-// fs.readFile("./docker-captains.html", "utf8", (err, text)=>{
-request.get("https://www.docker.com/community/docker-captains", (err, res, text) => {
-  let captains = parser.parse(text);
-  if(debug) {
-    console.log("|Captain|")
-  }
-
-  let valid = 0;
-  captains.sort((x,y) => {
-     if(x.text > y.text) {
-       return 1;
-     }
-     else if(x.text < y.text) {
-       return -1;
-     }
-     return 0;
-  }).forEach((cap) => {
-    if(cap.valid) {
-      modeCallback(cap);
-      valid++;
+    else {
+      work--;
     }
   });
+};
 
-  if(debug) {
-    console.log("\nCaptains: " + captains.length +", invalid handles: " + (captains.length - valid));
-  }
+let next = printer;
+if(options.mode=="locations") {
+  next = locationPrinter;
+}
+
+
+createList(next, () => {
+
 });
-
---
+++ optionsReader.js
@@ -0,0 +1,21 @@
+"use strict"
+
+module.exports = (argv) =>
+{
+    let options = {mode: "list"};
+    argv.map((elem) => {
+        let parts = elem.split("=");
+
+        if (parts[0] === "--debug") {
+          options.debug = true;
+        }
+        if (parts[0] === "--mode" && parts[1] === "locations") {
+            options.mode = "locations";
+        }
+        if (parts[0] === "--help") {
+            options.help = "help";
+        }
+    });
+
+    return options;
+};
